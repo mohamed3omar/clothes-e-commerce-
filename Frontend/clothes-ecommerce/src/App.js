b10664362b42c2f51d26aeb966c32cf756456a36
Abanoub-Samy13 <120567746+Abanoub-Samy13@users.noreply.github.com>
2022-12-24 00:19:44 +0200

    first step

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
+import React from 'react';
+import { ReactDOM } from 'react-dom';
 
+ReactDOM.render(<App/>, document.getElementById("root"));
 function App() {
   return (
     <>
---
+++ Frontend/clothes-ecommerce/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>clothes shop</div>
+  )
+}
+
+export default App
