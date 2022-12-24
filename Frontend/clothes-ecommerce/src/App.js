69f8ce15a46223ccccf871051d1158fef0aa4521
raniaashra <82020005+raniaashra@users.noreply.github.com>
2022-12-25 00:59:25 +0200

    Update App.js

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-
+import React from "react";
+import {BrowserRouter as  Router, Routes, Route, Link} from 'react-router-dom';
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
 function App() {
   return (
-    <>
-      just test
-    </>
+    <div>
+      <header className='App-header'>
+              <Router>
+                <Routes>
+                  <Route exact path='/' element ={<Login/>}></Route>
+                  <Route path="/Register" element ={<Register/>}></Route>
+                  
+                </Routes>
+              </Router>
+              <Home/>
+      </header>
+    </div>
   );
 }
 
