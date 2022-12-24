2fc2f72e3b7c9629e2904c091f5c9b017dc3fb0e
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-24 10:26:31 +0200

    update login register pages

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -1,24 +1,19 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as  Router, Routes, Route, Link} from 'react-router-dom';
+import '@fontsource/roboto/700.css';
+import {BrowserRouter as  Router, Routes, Route} from 'react-router-dom';
 import Login from './pages/Login'
 import Register from './pages/Register'
 function App() {
   return (
-    <div>
-      <header className='App-header'>
-              <Router>
-                <Routes>
-                  <Route exact path='/' element ={<Login/>}>
-                  
-                  </Route>
-                  <Route path="/Register" element ={<Register/>}>
-                  
-                  </Route>
-                </Routes>
-              </Router>
-      </header>
-    </div>
+        <>
+          <Router>
+            <Routes>
+              <Route exact path='/Login' element ={<Login/>}/>
+              <Route path="/Register" element ={<Register/>}/>
+            </Routes>
+          </Router>
+        </>
   );
 }
 
