33f492451564cbdeb1d437c952eb7ff92fe08984
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-24 13:22:45 +0200

    dashboard & navbar creation

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import React from "react";
 import '@fontsource/roboto/700.css';
-import {BrowserRouter as  Router, Routes, Route} from 'react-router-dom';
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { BrowserRouter as Router, Route  , Routes} from 'react-router-dom';
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Dashboard from './pages/dashboard/Dashboard'
+import Navbar from './component/Navbar';
 function App() {
   return (
-        <>
+     <>
+          <Navbar/>
           <Router>
             <Routes>
-              <Route exact path='/Login' element ={<Login/>}/>
-              <Route path="/Register" element ={<Register/>}/>
+                {/* <Route  path="/" element={} /> */}
+                <Route  path='/Login' element ={<Login/>}/>
+                <Route  path="/Register" element ={<Register/>}/>
+                <Route  path="/admin" element ={<Dashboard/> }/>
             </Routes>
           </Router>
-        </>
+     </>
   );
 }
 
