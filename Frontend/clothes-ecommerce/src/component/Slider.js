d199e630c656449315657c9de7e3334b82c0ef72
raniaashra <82020005+raniaashra@users.noreply.github.com>
2022-12-25 02:48:45 +0200

    Update Slider.js

--- Frontend/clothes-ecommerce/src/component/Slider.js
+++ Frontend/clothes-ecommerce/src/component/Slider.js
@@ -4,6 +4,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import clothes1 from '../assets/shopping (1).png';
 import clothes2 from '../assets/shopping (2).png';
+import clothes3 from '../assets/shopping-1.png';
+import clothes4 from'../assets/shopping.png';
+import clothes5 from'../assets/clothes5.webp';
+import cloth6 from'../assets/cloth6.webp';
+import clothes8 from'../assets/clothes8.webp';
 import { render } from "@testing-library/react";
 
 export default class Slider extends Component {
@@ -12,7 +17,7 @@ export default class Slider extends Component {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 5,
       slidesToScroll: 1
     };
   
@@ -22,21 +27,38 @@ export default class Slider extends Component {
         <Sliders {...settings}>
           <div>
             <img src={clothes1}/>
+            <h4>T-shirt</h4>
+          <span>100$</span>
           </div>
           <div>
             <img src ={clothes2}/>
+            <h4>blover</h4>
+          <span>150$</span>
           </div>
           <div>
-            <h3>3</h3>
+          <img src ={clothes3}/>
+          <h4>dress</h4>
+          <span>190$</span>
           </div>
           <div>
-            <h3>4</h3>
+          <img src ={clothes4}/>
+          <h4>Jacket</h4>
+          <span>150$</span>
           </div>
           <div>
-            <h3>5</h3>
+          <img src ={clothes5}/>
+          <h4>Jacket</h4>
+          <span>150$</span>
           </div>
           <div>
-            <h3>6</h3>
+          <img src ={cloth6}/>
+          <h4>dress</h4>
+          <span>250$</span>
+          </div>
+          <div >
+          <img src ={clothes8} width="280px"/>
+          <h4>Jacket</h4>
+          <span>150$</span>
           </div>
         </Sliders>
       </div>
