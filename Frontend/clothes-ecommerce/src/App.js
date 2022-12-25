d48649278cba6271323f99ef02dfade315d9de72
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-25 08:28:08 +0200

    Home page and update routes

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -5,21 +5,18 @@ import { BrowserRouter as Router, Route  , Routes} from 'react-router-dom';
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
 import Dashboard from './pages/dashboard/Dashboard'
-import Navbar from './component/Navbar';
-import Footer from './component/Footer';
+import Home from './pages/home/Home';
 function App() {
   return (
      <>
-          <Navbar/>
           <Router>
               <Routes>
-                  {/* <Route  path="/" element={} /> */}
+                  <Route  path="/" element={<Home/>} />
                   <Route  path='/Login' element ={<Login/>}/>
                   <Route  path="/Register" element ={<Register/>}/>
                   <Route  path="/admin" element ={<Dashboard/> }/>
               </Routes>
           </Router>
-          <Footer/>
      </>
   );
 }
