74b9363377cce66451ac1d217d932aa13eed5655
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-28 17:24:46 +0200

    Revert "first step"
    This reverts commit b10664362b42c2f51d26aeb966c32cf756456a36.

--- Frontend/clothes-ecommerce/src/App.js
+++ Frontend/clothes-ecommerce/src/App.js
@@ -1,8 +1,5 @@
 import './App.css';
-import React from 'react';
-import { ReactDOM } from 'react-dom';
 
-ReactDOM.render(<App/>, document.getElementById("root"));
 function App() {
   return (
     <>
