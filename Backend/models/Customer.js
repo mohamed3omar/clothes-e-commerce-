d6168aeccf7b9da7696dce22f3cc656b24e6c8fd
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-28 04:14:49 +0200

    Revert "create and connect database"
    This reverts commit 3905396a587509ea5a880780bccc4165646e951d.

--- Backend/models/Customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const CustomerSchema = mongoose.Schema({
-    Fname: String,
-    Lname : String,
-    Email: String,
-})
-
-export default mongoose.model('Customer' , CustomerSchema);
