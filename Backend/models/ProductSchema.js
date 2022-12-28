c74d5de5db1b2ae4272e4e2693684c3b207ffa34
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-12-28 13:05:25 +0200

    crud model for schema

--- Backend/models/ProductSchema.js
+++ Backend/models/ProductSchema.js
@@ -3,8 +3,6 @@ const ProductSchema = new mongoose.Schema({
     name:String,
     category:String,
     description:String,
-    created_at:{ type: Date },
-    updated_at:{ type: Date, default: Date.now },
-    updated:{type: Date, default: Date.now}
+    
 })
 export default mongoose.model('Product', ProductSchema);
